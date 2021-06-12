d87d0debb4fffd222d12206b6adef454299e1818
Yury Demidov <demidov_uriy@poisondrop.ru>
2021-06-12 09:19:19 +0300

    Add tests. Write simple server with browser test output. Update Readme

--- src/reduce.test.js
+++ src/reduce.test.js
@@ -1,19 +1,74 @@
-const assert = require('assert');
-const reduce = require('./reduce.ts');
-require('./shri-async-hw.js'); // imports globalThis.Homework
+import { assert } from 'chai/index.mjs';
+import reduce from './reduce.js';
+import './shri-async-hw.js'; // imports globalThis.Homework
 
-describe('reduce function for AsyncArray', function() {
-  it('Base function usage', function() {
-    const a = new Homework.AsyncArray([1, 2, 3]);
-    assert.strictEqual(reduce(
-      a,
-      (acc, cur, idx, src) => {
-        acc += cur + idx - 1;
-        src.print();
-        return acc;
-      },
-      0,
-      result => console.log(result)
-    ), 6);
+describe('reduce function for AsyncArray', () => {
+  describe('Base function usage', () => {
+    it('Total numbers sum reducer', done => {
+      const arr = new Homework.AsyncArray([1, 2, 3]);
+      reduce(
+        arr,
+        (acc, cur, idx, src) => {
+          if (src instanceof Homework.AsyncArray) {
+            acc += cur + idx - 1;
+          }
+          return acc;
+        },
+        0,
+        result => {
+          assert.strictEqual(result, 6);
+          done();
+        }
+      );
+    });
+
+    it('String concatenation reducer', done => {
+      const arr = new Homework.AsyncArray('Hello'.split(''));
+      reduce(
+        arr,
+        (acc, cur) => {
+          acc += `${cur}-`;
+          return acc;
+        },
+        '-',
+        result => {
+          assert.strictEqual(result, '-H-e-l-l-o-');
+          done();
+        }
+      );
+    });
+
+    it('Array-to-array reducer', done => {
+      const arr = new Homework.AsyncArray([1, true, 'hello', [1]]);
+      reduce(
+        arr,
+        (acc, cur) => {
+          acc.push(typeof cur === 'string' ? cur : JSON.stringify(cur));
+          return acc;
+        },
+        [],
+        result => {
+          assert.deepEqual(result, ['1', 'true', 'hello', '[1]']);
+          done();
+        }
+      );
+    });
+  });
+
+  describe('Edge cases', () => {
+    it('Empty array', done => {
+      const arr = new Homework.AsyncArray([]);
+      reduce(
+        arr,
+        (acc, cur) => {
+          return acc += cur;
+        },
+        0,
+        result => {
+          assert.deepEqual(result, 0);
+          done();
+        }
+      );
+    });
   });
 });
--- src/test-output.js
+++ src/test-output.js
@@ -1,15 +1,64 @@
 import reduce from './reduce.js';
 
-const a = new Homework.AsyncArray([1, 2, 3]);
+console.log('reduce function for AsyncArray');
 
-console.log('Функция reduce для AsyncArray')
+console.log('START Total numbers sum reducer');
+const arr1 = new Homework.AsyncArray([1, 2, 3]);
 reduce(
-  a,
+  arr1,
   (acc, cur, idx, src) => {
-    acc += cur + idx - 1;
-    src.print();
+    if (src instanceof Homework.AsyncArray) {
+      acc += cur + idx - 1;
+    }
     return acc;
   },
   0,
-  result => console.log(result)
+  result => {
+    console.log(`END Total numbers sum reducer: ${result}`)
+  }
 );
+
+console.log('START String concatenation reducer');
+const arr2 = new Homework.AsyncArray('Hello'.split(''));
+reduce(
+  arr2,
+  (acc, cur) => {
+    acc += `${cur}-`;
+    return acc;
+  },
+  '-',
+  result => {
+    console.log(`END String concatenation reducer: ${result}`)
+  }
+);
+
+console.log('START Array-to-array reducer');
+const arr3 = new Homework.AsyncArray([1, true, 'hello', [1]]);
+reduce(
+  arr3,
+  (acc, cur) => {
+    acc.push(typeof cur === 'string' ? cur : JSON.stringify(cur));
+    return acc;
+  },
+  [],
+  result => {
+    console.log(`END Array-to-array reducer: ${result}`)
+  }
+);
+
+console.log('START Empty array');
+const arr4 = new Homework.AsyncArray([]);
+reduce(
+  arr4,
+  (acc, cur) => {
+    return acc += cur;
+  },
+  0,
+  result => {
+    console.log(`END Empty array: ${result}`)
+  }
+);
+
+fetch('/test-output.js')
+  .then(response => response.text())
+  .then(data => document.querySelector('#output').textContent = data);
